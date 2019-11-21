f896720becabdff61a02bb5fb9c544f7a5a99158
Andyy <andrewbergheim@gmail.com>
2019-11-21 13:41:20 -0800

    div organization and script to write  quiz outline

--- quiz app/script.js
+++ quiz app/script.js
@@ -5,18 +5,42 @@
         //initialize timer variable
         let TimeRemaining = 75;
         
+         //initialize quiz html here
+         let quizDiv = document.getElementById("quiz-content");
+         let mainQuestionHead = document.createElement("h1");
+         mainQuestionHead.setAttribute("id","current-question")
+
+         let AnswerOneButton = document.createElement("button");
+         AnswerOneButton.setAttribute("id","answer-1");
+
+         let AnswerTwoButton = document.createElement("button");
+         AnswerTwoButton.setAttribute("id","answer-2");
+
+         let AnswerThreeButton = document.createElement("button");
+         AnswerThreeButton.setAttribute("id","answer-3");
+
+         let AnswerFourButton = document.createElement("button");
+         AnswerFourButton.setAttribute("id", "answer-4");
+
+         quizDiv.appendChild(mainQuestionHead);
+         quizDiv.appendChild(AnswerOneButton);
+         quizDiv.appendChild(AnswerTwoButton);
+         quizDiv.appendChild(AnswerThreeButton);
+         quizDiv.appendChild(AnswerFourButton);
+
         // start timer
         var timer = setInterval(function(){
         document.getElementById("time").innerHTML = TimeRemaining;
              TimeRemaining--;
-                    if (TimeRemaining < 0){
-                        clearInterval(timer);
-                        //write score page html here
-                }
-                //initialize quiz html here
-                
+                    
+               
                 //quiz conditionals
 
+                if (TimeRemaining < 0){
+                    clearInterval(timer);
+                    //write score page html here
+                    
+            }
 
             },1000);
         //add question
