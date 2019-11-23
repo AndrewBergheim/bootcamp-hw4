998aed26dc6e624a53ba91c3b4ce833d31112bd1
Andyy <andrewbergheim@gmail.com>
2019-11-23 14:54:42 -0800

    button classes, mostly working correct answers (click limiter WIP)

--- quiz app/script.js
+++ quiz app/script.js
@@ -1,4 +1,5 @@
 
+    
     function quiz(){
         // Remove button
         document.getElementById("starting-content").remove();
@@ -13,15 +14,19 @@
 
          let AnswerOneButton = document.createElement("button");
          AnswerOneButton.setAttribute("id","answer-1");
+         AnswerOneButton.setAttribute("class","answer-button correct-answer"); // first answer is correct
 
          let AnswerTwoButton = document.createElement("button");
          AnswerTwoButton.setAttribute("id","answer-2");
+         AnswerTwoButton.setAttribute("class","answer-button");
 
          let AnswerThreeButton = document.createElement("button");
          AnswerThreeButton.setAttribute("id","answer-3");
+         AnswerThreeButton.setAttribute("class","answer-button");
 
          let AnswerFourButton = document.createElement("button");
          AnswerFourButton.setAttribute("id", "answer-4");
+         AnswerFourButton.setAttribute("class","answer-button");
 
          quizDiv.appendChild(mainQuestionHead);
          quizDiv.appendChild(AnswerOneButton);
@@ -30,37 +35,109 @@
          quizDiv.appendChild(AnswerFourButton);
 
          //variable declarations for easy access later
-       let QuestionContent = document.getElementById("current-question")
-        let Answer1Content = document.getElementById("answer-1")
-        let Answer2Content = document.getElementById("answer-2")
-        let Answer3Content = document.getElementById("answer-3")
-        let Answer4Content = document.getElementById("answer-4")
+       let QuestionContent = document.getElementById("current-question");
+        let Answer1Content = document.getElementById("answer-1") ;
+        let Answer2Content = document.getElementById("answer-2");
+        let Answer3Content = document.getElementById("answer-3");
+        let Answer4Content = document.getElementById("answer-4");
 
         // start timer
-        var timer = setInterval(function(){
+        let timer = setInterval(function(){
         document.getElementById("time").innerHTML = TimeRemaining;
-             TimeRemaining--;
-                    
+            let answered = false;
+            answered = false;
+            TimeRemaining--;
                
                 //quiz conditionals
 
                 if (TimeRemaining < 0){
                     clearInterval(timer);
-                    //write score page html here
+                    //write loss page
                     
             }
             //add question
-            if (questionCount = 1){
+            if (questionCount === 1){
                 QuestionContent.innerHTML = question1.question
-                Answer1Content.innerHTML = question1.answer1
+                Answer1Content.innerHTML = question1.answer1 // correct
                 Answer2Content.innerHTML = question1.answer2
                 Answer3Content.innerHTML = question1.answer3
                 Answer4Content.innerHTML = question1.answer4
+
+                //function def for event listener
+
+                
+
+                // event listener for correct answer
+                let answerTarget = document.getElementsByClassName("answer-button");
+                let Listener1 = answerTarget[0].addEventListener("click", function(){
+                    if (answered === false){
+                        answered = true;
+                        if (questionCount === 1)
+                        questionCount+= 1;
+                        //remove event listener
+                        
+                        document.removeEventListener("click", Listener1) 
+                        }
+                        else{
+                            console.log("prevented from firing twice")
+                        }
+                    });
+                }
+
+            if (questionCount === 2){
+                QuestionContent.innerHTML = question2.question
+                Answer1Content.innerHTML = question2.answer1 
+                Answer2Content.innerHTML = question2.answer2
+                Answer3Content.innerHTML = question2.answer3
+                Answer4Content.innerHTML = question2.answer4// correct
+
+                let answerTarget = document.getElementsByClassName("answer-button");
+                answerTarget[3].addEventListener("click", function(){
+                    if (answered === false){
+                        answered = true;
+                        if (questionCount === 1)
+                    questionCount+= 1;
+
+                    }
+                    else{
+                        console.log("prevented from firing twice")
+                    }
+                });
             }
 
+            if (questionCount === 3){
+                QuestionContent.innerHTML = question3.question
+                Answer1Content.innerHTML = question3.answer1 
+                Answer2Content.innerHTML = question3.answer2 //correct
+                Answer3Content.innerHTML = question3.answer3
+                Answer4Content.innerHTML = question3.answer4
+            }
+
+            if (questionCount === 4){
+                QuestionContent.innerHTML = question4.question
+                Answer1Content.innerHTML = question4.answer1 
+                Answer2Content.innerHTML = question4.answer2
+                Answer3Content.innerHTML = question4.answer3 // correct
+                Answer4Content.innerHTML = question4.answer4
+            }
+
+            if (questionCount === 5){
+                QuestionContent.innerHTML = question5.question // correct
+                Answer1Content.innerHTML = question5.answer1 
+                Answer2Content.innerHTML = question5.answer2
+                Answer3Content.innerHTML = question5.answer3
+                Answer4Content.innerHTML = question5.answer4
+            }
+
+            if (questionCount > 5){
+                let Score = TimeRemaining;
+                clearInterval(timer);
+                
+                //write high score page
+            }
+            console.log(questionCount);
+
             },1000);
-        
-        
     }
 
     
